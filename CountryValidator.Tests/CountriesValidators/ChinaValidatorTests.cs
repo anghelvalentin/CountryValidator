5a7c51c054e04ccee6f63381e5295abf0e081f7e
Tony Qu <tonyqus@gmail.com>
2021-10-24 13:11:11 +0800

    add ChinaValidatorTests

---
+++ CountryValidator.Tests/CountriesValidators/ChinaValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CountryValidation.Countries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CountryValidator.Tests.CountriesValidators
+{
+    public class ChinaValidatorTests
+    {
+        private readonly ChinaValidator _chinaValidator;
+
+        public ChinaValidatorTests()
+        {
+            _chinaValidator = new ChinaValidator();
+        }
+
+        [Theory]
+        [InlineData("342122198211155230", true)]
+        [InlineData("810000199408230021", true)]
+        [InlineData("830000199201300022", true)]
+        [InlineData("810000199408230023", false)]
+        public void TestNationalId_18digits(string code, bool isValid)
+        {
+            Assert.Equal(isValid, _chinaValidator.ValidateNationalIdentity(code).IsValid);
+        }
+
+    }
+}
