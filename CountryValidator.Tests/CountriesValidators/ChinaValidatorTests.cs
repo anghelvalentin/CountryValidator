bb18d89ae511cef1bbb45f92675da2672cd46675
Tony Qu <tonyqus@gmail.com>
2021-10-27 11:10:52 +0800

    implement ValidateEntity and add test cases

--- CountryValidator.Tests/CountriesValidators/ChinaValidatorTests.cs
+++ CountryValidator.Tests/CountriesValidators/ChinaValidatorTests.cs
@@ -16,9 +16,12 @@ namespace CountryValidator.Tests.CountriesValidators
         }
 
         [Theory]
+        [InlineData("360402198611133850", true)]
         [InlineData("342122198211155230", true)]
+        [InlineData("220211198701291224", true)]
         [InlineData("810000199408230021", true)]
         [InlineData("830000199201300022", true)]
+        [InlineData("420400197512034215", true)]
         [InlineData("810000199408230023", false)]
         public void TestNationalId_18digits(string code, bool isValid)
         {
@@ -32,5 +35,26 @@ namespace CountryValidator.Tests.CountriesValidators
         {
             Assert.Equal(isValid, _chinaValidator.ValidateNationalIdentity(code).IsValid);
         }
+
+        [Theory]
+        [InlineData("9111010878398835XQ", true)]
+        [InlineData("91310110MA1G8KLD1C", true)]
+        [InlineData("91610000719782242D", true)]
+        [InlineData("91420100MA49NUAH2W", true)]
+        [InlineData("91420000706803542C", true)]
+        [InlineData("91310116594798880J", true)]
+        [InlineData("91110108600040399G", true)]
+        [InlineData("51420000MJH2042495", true)]
+        [InlineData("51420000MJH204142R", true)]
+        [InlineData("51420000MJH2006517", true)]
+        [InlineData("53420000MJH24497XN", true)]
+        [InlineData("113100000024280004", true)]
+        [InlineData("11310116002475827M", true)]        
+        public void TestEntityCode(string code, bool isValid)
+        {
+            Assert.Equal(isValid, _chinaValidator.ValidateEntity(code).IsValid);
+        }
+
+
     }
 }
--- CountryValidator/CountriesValidators/ChinaValidator.cs
+++ CountryValidator/CountriesValidators/ChinaValidator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Text.RegularExpressions;
 
@@ -6,7 +7,43 @@ namespace CountryValidation.Countries
 {
     public class ChinaValidator : IdValidationAbstract
     {
-        private int GetWeight(int n)
+        static readonly Dictionary<char, int> charToNumDict = new Dictionary<char, int>();
+        static ChinaValidator()
+        {
+            charToNumDict.Add('0', 0);
+            charToNumDict.Add('1', 1);
+            charToNumDict.Add('2', 2);
+            charToNumDict.Add('3', 3);
+            charToNumDict.Add('4', 4);
+            charToNumDict.Add('5', 5);
+            charToNumDict.Add('6', 6);
+            charToNumDict.Add('7', 7);
+            charToNumDict.Add('8', 8);
+            charToNumDict.Add('9', 9);
+            charToNumDict.Add('A', 10);
+            charToNumDict.Add('B', 11);
+            charToNumDict.Add('C', 12);
+            charToNumDict.Add('D', 13);
+            charToNumDict.Add('E', 14);
+            charToNumDict.Add('F', 15);
+            charToNumDict.Add('G', 16);
+            charToNumDict.Add('H', 17);
+            charToNumDict.Add('J', 18);
+            charToNumDict.Add('K', 19);
+            charToNumDict.Add('L', 20);
+            charToNumDict.Add('M', 21);
+            charToNumDict.Add('N', 22);
+            charToNumDict.Add('P', 23);
+            charToNumDict.Add('Q', 24);
+            charToNumDict.Add('R', 25);
+            charToNumDict.Add('T', 26);
+            charToNumDict.Add('U', 27);
+            charToNumDict.Add('W', 28);
+            charToNumDict.Add('X', 29);
+            charToNumDict.Add('Y', 30);
+        }
+
+        private int GetNationalIDWeight(int n)
         {
             return (int)Math.Pow(2, n - 1) % 11;
         }
@@ -56,7 +93,7 @@ namespace CountryValidation.Countries
                 for (int i = 0, len = identifier.Length; i < len; i++)
                 {
                     _char = identifier[i].ToString();
-                    weightedSum += int.Parse(_char) * GetWeight(index);
+                    weightedSum += int.Parse(_char) * GetNationalIDWeight(index);
                     index--;
                 }
 
@@ -89,9 +126,51 @@ namespace CountryValidation.Countries
             }
         }
 
+        static readonly int[] EntityCodeWeightingFactors = { 1, 3, 9, 27, 19, 26, 16, 17, 20, 29, 25, 13, 8, 24, 10, 30, 28 };
+        /// <summary>
+        /// This method is used for Entity validation only
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private int CharToNum(char c)
+        {
+            if (charToNumDict.ContainsKey(c))
+                return charToNumDict[c];
+            return -1;
+        }
+        /// <summary>
+        /// Validate Uniform Social Credit Code
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public override ValidationResult ValidateEntity(string id)
         {
-            throw new NotImplementedException();
+            id = id.RemoveSpecialCharacthers();
+            if (id.Length != 18)
+            {
+                return ValidationResult.Invalid("Invalid length! The code must have a length of 18");
+            }
+            if (!Regex.IsMatch(id,"^[159Y]{1}[1239]{1}[0-9]{6}[^_IOZSVa-z\\W]{10}$"))
+            {
+                return ValidationResult.Invalid("Invalid format");
+            }
+            int tempSum = 0;
+
+            for (var i = 0; i <17; i++)
+            {
+                var t = CharToNum(id[i]);
+                if (t == -1)
+                    return ValidationResult.InvalidFormat(string.Format("There is a invalid character '{0}' at position {1}",id[i],i+1));
+
+                tempSum += EntityCodeWeightingFactors[i] * t;
+            }
+            var checksum = 31 - tempSum % 31;
+            if (checksum == 31)
+                checksum = 0;
+            if (checksum == this.CharToNum(id[17]))
+                return ValidationResult.Success();
+            else
+                return ValidationResult.InvalidChecksum();
         }
 
         public override ValidationResult ValidateVAT(string vatId)
